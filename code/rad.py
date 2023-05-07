8a341c5fbc367a5076b7e0c3e82f2984b369d04e
Andi388918 <38116053+Andi388918@users.noreply.github.com>
2023-05-07 13:21:23 +0200

    Add files via upload

--- code/rad.py
+++ code/rad.py
@@ -44,7 +44,32 @@ ax1.set_title("Charakteristik der Geigerzählröhre")
 ax1.grid(linewidth = 0.2)
 ax1.legend(loc="upper left")
 
-# Exercise 2: Activity of Ba 137 *
+# Exercise 2: Background radiation
+
+directory_name = "data_rs\A2\Hintergrundmessung_1100V_15_sec.txt"
+bgr_times = []
+bgr_activities = []
+
+with open(directory_name, "r") as file:
+        lines = file.readlines()
+        for line in lines[4:]:
+             if line.strip() != "":
+                t, a = map(int, line.split())
+                bgr_times.append(t)
+                bgr_activities.append(ufloat(a, 4 * np.sqrt(a/4)))
+
+bgr_activities = np.array(bgr_activities)
+mean_bgr_activity = np.mean(bgr_activities)
+bgr_activities_15_sec_interval = bgr_activities/4
+
+fig, ax2 = plt.subplots()
+ax2.hist(unp.nominal_values(bgr_activities_15_sec_interval), density=True, bins=30)
+ax2.set_xlabel(r"Zählrate $A$ in (15 s)$^{-1}$")
+ax2.set_ylabel(r"Häufigkeit")
+ax2.set_title(r"Statistik radioaktiver Zerfälle (Hintergrundstrahlung)")
+ax2.grid(linewidth = 0.2)
+
+# Exercise 3: Activity of Ba 137 *
 
 directory_name = "data_rs\A2\Ba_15sec_1100V.txt"
 times = []
@@ -59,15 +84,15 @@ with open(directory_name, "r") as file:
                 activities.append(ufloat(a, 4 * np.sqrt(a/4)))
 
 times = np.array(times)/1e3
-activities = np.array(activities)/60
+activities = (np.array(activities) - mean_bgr_activity)/60
 
-fig, ax2 = plt.subplots()
-ax2.errorbar(x=times, y=unp.nominal_values(activities), xerr=0, yerr=unp.std_devs(activities), fmt=".", label="Messdaten")
-ax2.set_xlabel(r"Zeit $t$ in s")
-ax2.set_ylabel(r"Log. Aktivität $log(A)$ [dimensionslos]")
-ax2.set_title(r"Zerfall des $^{137}$Ba$^*$ Isotops")
-ax2.grid(linewidth = 0.2)
-ax2.set_yscale('log')
+fig, ax3 = plt.subplots()
+ax3.errorbar(x=times, y=unp.nominal_values(activities), xerr=0, yerr=unp.std_devs(activities), fmt=".", label="Messdaten")
+ax3.set_xlabel(r"Zeit $t$ in s")
+ax3.set_ylabel(r"Log. Aktivität $log(A)$ [dimensionslos]")
+ax3.set_title(r"Zerfall des $^{137}$Ba$^*$ Isotops")
+ax3.grid(linewidth = 0.2)
+ax3.set_yscale('log')
 
 def linear(x, a, c):
      return a*x + c
@@ -78,8 +103,8 @@ lambda_c = -ufloat(popt[0], np.sqrt(np.diag(pcov))[0])
 half_life = np.log(2)/lambda_c
 
 x_fit = np.linspace(0, max(times), 1000)
-ax2.plot(x_fit, np.exp(linear(x_fit, popt[0], popt[1])), label="Fitfunktion $y = -\lambda \cdot x + c$")
-ax2.legend(loc="upper right")
+ax3.plot(x_fit, np.exp(linear(x_fit, popt[0], popt[1])), label="Fitfunktion $y = -\lambda \cdot x + c$")
+ax3.legend(loc="upper right")
 
 print(f"half life = {half_life} s")
 
